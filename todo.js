9b8535b9fc01546e418638c7b0da534a2e864f7d
hyoilll <50327128+hyoilll@users.noreply.github.com>
2020-06-21 00:40:01 +0900

    2020/06/21 js end #weather #background

--- todo.js
+++ todo.js
@@ -4,7 +4,25 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+  const li = event.target.parentNode;
+  toDoList.removeChild(li);
+
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id != parseInt(li.id);
+  });
+
+  // for (let i = 0; i < toDos.length; ++i) {
+  //   if (toDos[i].id == li.id) {
+  //     toDos.splice(i, 1);
+  //   }
+  // }
+
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   //JSON.stringify(toDos) array의 내용물을 string으로 변환
@@ -13,17 +31,19 @@ function saveToDos() {
 
 function paintToDo(text) {
   const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  delBtn.innerText = "delete";
 
   const span = document.createElement("span");
   span.innerText = text;
 
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "delete";
+  delBtn.addEventListener("click", deleteToDo);
+
   const newId = toDos.length + 1;
 
   //()의 값을 li의 자식으로 넣음.
-  li.appendChild(delBtn);
   li.appendChild(span);
+  li.appendChild(delBtn);
 
   li.id = newId;
 
@@ -70,3 +90,92 @@ function init() {
 }
 
 init();
+
+// const toDoForm = document.querySelector(".js-toDOForm");
+// const toDoInput = toDoForm.querySelector("input");
+// const toDoList = document.querySelector(".js-toDoList");
+
+// const TODOS_LS = "toDos";
+
+// const toDos = [];
+
+// function deleteToDos(item) {
+//   for (let i = 0; i < toDos.length; ++i) {
+//     if (toDos[i].id == item.id) {
+//       toDos.splice(i, 1);
+//     }
+//   }
+// }
+
+// function deleteToDo(event) {
+//   const li = event.target.parentNode;
+//   toDoList.removeChild(li);
+//   deleteToDos(li);
+//   saveToDos();
+// }
+
+// function saveToDos() {
+//   //JSON.stringify(toDos) array의 내용물을 string으로 변환
+//   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+// }
+
+// function paintToDo(text) {
+//   const li = document.createElement("li");
+//   const delBtn = document.createElement("button");
+//   delBtn.innerText = "delete";
+//   delBtn.addEventListener("click", deleteToDo);
+
+//   const span = document.createElement("span");
+//   span.innerText = text;
+
+//   const newId = toDos.length + 1;
+
+//   //()의 값을 li의 자식으로 넣음.
+//   li.appendChild(delBtn);
+//   li.appendChild(span);
+
+//   li.id = newId;
+
+//   toDoList.appendChild(li);
+
+//   const toDoObj = {
+//     text: text,
+//     id: newId,
+//   };
+//   toDos.push(toDoObj);
+
+//   //localStorage에 list저장
+//   saveToDos();
+// }
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const currentValue = toDoInput.value;
+//   paintToDo(currentValue);
+//   toDoInput.value = "";
+// }
+
+// function loadToDos() {
+//   const loadedToDos = localStorage.getItem(TODOS_LS);
+
+//   if (loadedToDos != null) {
+//     //string to object
+//     const parsedToDos = JSON.parse(loadedToDos);
+
+//     // parsedToDos.forEach(function (toDo) {
+//     //   paintToDo(toDo.text);
+//     // });
+
+//     for (let i = 0; i < parsedToDos.length; ++i) {
+//       paintToDo(parsedToDos[i].text);
+//     }
+//   }
+// }
+
+// function init() {
+//   loadToDos();
+
+//   toDoForm.addEventListener("submit", handleSubmit);
+// }
+
+// init();
---
+++ weather.js
@@ -0,0 +1,59 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "21bda8e399d110518c102ec4ec22ffa5"; //weather api
+const COORDS = "coords";
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  const coordsObj = {
+    latitude: latitude,
+    longitude: longitude,
+  };
+
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Error");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+
+  if (loadedCoords == null) {
+    askForCoords();
+  } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
